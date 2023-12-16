83d9951f5df26f0efe5d89238c18701a6c920329
ile <ilezdravev1@gmail.com>
2023-12-16 13:59:50 +0100

    init

---
+++ precache-manifest.e0eaabd7bc6fe216ffe8de2eb456db6d.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74593c2efd656661848ddae7a6aee451",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8919ab7a412e1254e098",
+    "url": "/static/css/main.b1204bcd.chunk.css"
+  },
+  {
+    "revision": "fec31459e8643889b99e",
+    "url": "/static/js/2.b63c552c.chunk.js"
+  },
+  {
+    "revision": "84f48c9a090ba2f8cc4a11e48dd3dad0",
+    "url": "/static/js/2.b63c552c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8919ab7a412e1254e098",
+    "url": "/static/js/main.1e7616ae.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "7c8e5f50725885d51b72b45164274e41",
+    "url": "/static/media/avatar.7c8e5f50.png"
+  },
+  {
+    "revision": "2dd213aa54b22c3bca0c62f570d1512a",
+    "url": "/static/media/coffee-apple.2dd213aa.jpg"
+  },
+  {
+    "revision": "54fd423489a1937caf8950d12c8a8526",
+    "url": "/static/media/html-css-javascript-lg.54fd4234.jpg"
+  },
+  {
+    "revision": "2c7701aae07adecdb0a7e336dff08def",
+    "url": "/static/media/html-css-javascript.2c7701aa.jpg"
+  },
+  {
+    "revision": "730958a228d26dee011c5aa0aa3c3db9",
+    "url": "/static/media/javascript-fullstack.730958a2.jpg"
+  },
+  {
+    "revision": "90616a5dc9acdcce7158addc89620052",
+    "url": "/static/media/mern-stack.90616a5d.jpg"
+  },
+  {
+    "revision": "a8b012a0d7afd08ee07251e1f67294ff",
+    "url": "/static/media/react-redux.a8b012a0.jpg"
+  },
+  {
+    "revision": "f370c1e0adb80194dd3ea10e468bc69a",
+    "url": "/static/media/react.f370c1e0.png"
+  }
+]);
